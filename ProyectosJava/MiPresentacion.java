3ab6d621e3141fcf200b17006a9545d77466b11f
kak001 <joaco.atj06@gmail.com>
2026-08-13 11:39:28 -0400

    Mas test

--- ProyectosJava/MiPresentacion.java
+++ ProyectosJava/MiPresentacion.java
@@ -4,30 +4,21 @@ public class MiPresentacion {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-
-        System.out.println("Ingresa tu nombre: ");
+        System.out.println("Ingresa tu nombre:");
         String nombre = teclado.nextLine();
 
         System.out.println("Ingresa tu edad: ");
         int edad = teclado.nextInt();
         teclado.nextLine();
 
-        System.out.println("Ingresa la carrera que estas estudiando: ");
+        System.out.println("Ingresa tu carrera: ");
         String carrera = teclado.nextLine();
 
-        System.out.println("Ingresa el semestre que estas cursando: ");
-        int semestre = teclado.nextInt();
-        teclado.nextLine();
-
-        System.out.println("Registrando datos del estudiante...");
-        System.out.println();
-
-        System.out.println("=======================================");
-        System.out.println("Estudiante: " + nombre);
+        System.out.println("========DATOS DEL ESTUDIANTE========");
+        System.out.println("Nombre: " + nombre);
         System.out.println("Edad: " + edad);
         System.out.println("Carrera: " + carrera);
-        System.out.println("Año 2026, semestre: " + semestre);
-        System.out.println("=======================================");
+        System.out.println("====================================");
 
         teclado.close();
     }
