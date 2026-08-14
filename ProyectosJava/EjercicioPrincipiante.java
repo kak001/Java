f2434f376c911fc1c1cc61be8aa9e436ab12e151
kak001 <joaco.atj06@gmail.com>
2026-08-13 21:33:57 -0400

    Mas ejercicios .java

---
+++ ProyectosJava/EjercicioPrincipiante.java
@@ -0,0 +1,45 @@
+public class EjercicioPrincipiante {
+    public static void main(String[] args) {
+        String name = "kako";
+        System.out.println("Nombre: " + name);
+
+        int age = 20;
+        System.out.println("Edad: " + age);
+
+        double height = 1.8;
+        System.out.println(height + " Metros de altura");
+
+        boolean myBoolean = true;
+        System.out.println("¿Me gusta programar?: " + myBoolean);
+
+        final String EMAIL = "joaco.atj06@gmail.com";
+        System.out.println("Mi correo electronico: " + EMAIL);
+
+        char myChar = 'J';
+        System.out.println("Mi inicial: " + myChar);
+
+        String local = "Puente Alto";
+        System.out.println(local);
+
+        local = "Peñalolen";
+        System.out.println(local);
+
+        int a = 10;
+
+        int b = 11;
+
+        System.out.println(a + b);
+
+        Integer number = 67;
+
+        System.out.println(name.getClass().getSimpleName());
+
+        System.out.println(number.getClass().getSimpleName());
+
+        int j;
+
+        j = 10;
+
+        System.out.println(j);
+    }
+}
