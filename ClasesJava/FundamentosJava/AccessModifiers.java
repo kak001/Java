4ada128abecba4bdf5d652e5960f553824a79218
kak001 <joaco.atj06@gmail.com>
2026-08-25 21:07:23 -0400

    mod archivo + .java

---
+++ ClasesJava/FundamentosJava/AccessModifiers.java
@@ -0,0 +1,14 @@
+public class AccessModifiers {
+    public static void main(String[] args) {
+        
+        // Encapsulamiento (modificadores de acceso)
+
+        // Modificadores de acceso
+        // public
+        // private
+        // protected
+        // (default)
+
+        
+    }
+}
