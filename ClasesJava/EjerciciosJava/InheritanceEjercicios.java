44425da036c5f0570e42b074dec35824952a1753
kak001 <joaco.atj06@gmail.com>
2026-08-27 22:50:17 -0400

    .java añadido

---
+++ ClasesJava/EjerciciosJava/InheritanceEjercicios.java
@@ -0,0 +1,271 @@
+public class InheritanceEjercicios {
+    public static void main(String[] args) {
+
+        // run-code
+
+        // 1.
+        // Super Class
+        var vehicle = new Vehicle("Porsche", "911 GT3 RS", "Gris Mate");
+        vehicle.move();
+
+        // Sub Class
+        var car = new Car("Mercedes-Benz AMG", "GT Black Series", "Naranjo");
+        car.honkNoise();
+        System.out.println();
+
+        // 2.
+        // Sub class
+        var student = new Student("kako", 20, 7.0);
+        student.study();
+        System.out.println();
+
+        // 3.
+        // Super class
+        var animal = new Animal("Arenita");
+        animal.makeSound();
+
+        // Sub classes
+        var dog = new Dog("Tobby");
+        dog.makeSound();
+
+        var cat = new Cat("Miku");
+        cat.makeSound();
+        System.out.println();
+
+        // 4.
+        var employee = new Employee("kako", 2500000);
+        employee.employeeInfo();
+
+        var manager = new Manager("Joaquin", 3500000, "Ciberseguridad");
+        manager.employeeInfo();
+        System.out.println();
+
+        // 5.
+        var circle = new Circle(5);
+        System.out.println("El area de un circulo de radio 5 es: " + circle.calculateArea() + " cm^2.");
+
+        var rectangle = new Rectangle(3, 5);
+        System.out.println("El area de un rectangulo ancho 3 y altura 5 es: " + rectangle.calculateArea() + " cm^2.");
+    }
+
+    // 1. Crea una clase Vehicle con un método move(). Luego crea una subclase Car que herede de Vehicle y agrega el método honk().
+    // Super class
+    public static class Vehicle {
+        
+        //Atributos
+        String brand;
+        String model;
+        String color;
+
+        // Constructor
+        public Vehicle(String brand, String model, String color) {
+            this.brand = brand;
+            this.model = model;
+            this.color = color;
+        }
+
+        // Metodos
+        public void move() {
+            System.out.println("El vehiculo se esta movimiendo.");
+        }
+    }
+
+    // Sub class
+    public static class Car extends Vehicle {
+
+        // Constructor
+        public Car(String brand, String model, String color) {
+            super(brand, model, color);
+        }
+
+        // Metodos
+        public void honkNoise() {
+            System.out.println("¡HONK!");
+        }
+    }
+
+    // 2. Define una clase Person con los atributos name y age. Luego crea una clase Student que agregue el atributo grade y un método study().
+    // Super class
+    public static class Person {
+
+        // Atributos
+        String name;
+        int age;
+
+        // Constructor
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+    }
+
+    // Sub class
+    public static class Student extends Person {
+
+        // Atributos
+        double grade;
+
+        // Constructor
+        public Student(String name, int age, double grade) {
+            super(name, age);
+            this.grade = grade;
+        }
+
+        // Metodos
+        public void study() {
+            System.out.println("El alumno " + name + " de " + age + " de edad esta estudiando para poder alzanzar una nota " + grade + " en programacion orientada a objetos.");
+        }
+    }
+    
+    // 3. Crea una clase Animal con el método makeSound(). Haz que Dog diga “Woof” y Cat diga “Meow” sobrescribiendo ese método.
+    // Super class
+    public static class Animal {
+
+        // Atributos
+        private String name;
+
+        // Constructor
+        public Animal(String name) {
+            this.name = name;
+        }
+
+        // Getters
+        public String getName() {
+            return name;
+        }
+
+        // Setters
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        // Metodos
+        public void makeSound() {
+            System.out.println("El animal de nombre " + name + " esta haciendo un sonido propio generico.");
+        }
+    }
+
+    // Sub classes
+    public static class Dog extends Animal {
+
+        // Constructor
+        public Dog(String name) {
+            super(name);
+        }
+
+        // Metodos
+        @Override
+        public void makeSound() {
+            System.out.println("El perro " + getName() + " esta ladrando: ¡Woof!");
+        }
+    }
+
+    public static class Cat extends Animal {
+
+        // Constructor
+        public Cat(String name) {
+            super(name);
+        }
+
+        // Metodos
+        @Override
+        public void makeSound() {
+            System.out.println("El gato " + getName() + " esta maullando: ¡Meow!");
+        }
+    }
+    
+    // 4. La clase Employee tiene los atributos name y salary. Manager hereda de Employee y agrega el atributo department.
+    // Super class
+    public static class Employee {
+
+        private String name;
+        private int salary;
+
+        public Employee(String name, int salary){
+            this.name = name;
+            this.salary = salary;
+        }
+
+        public String getName() {
+            return name;
+        }
+         
+        public int getSalary() {
+            return salary;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setSalary(int salary) {
+            this.salary = salary;
+        }
+
+        public void employeeInfo() {
+            System.out.println("El trabajador " + name + " gana un salario mensual de: $" + salary + " pesos chilenos.");
+        }
+    }
+
+    //Sub class
+    public static class Manager extends Employee {
+
+        private String department;
+
+        public Manager(String name, int salary, String department) {
+            super(name, salary);
+            this.department = department;
+        }
+
+        public String getDepartment() {
+            return department;
+        }
+
+        public void setDepartment(String department) {
+            this.department = department;
+        }
+
+        @Override
+        public void employeeInfo() {
+            System.out.println("El trabajador " + getName() + " trabaja en el departamento de " + getDepartment() + " ganando mensualmente: $" + getSalary() + " pesos chilenos.");
+        }
+    }
+    
+    // 5. Crea una clase abstracta Shape con un método calculateArea(). Luego implementa ese método en Circle y Rectangle.
+    // Abstract class
+
+    static abstract class Shape {
+        public abstract double calculateArea();
+    }
+
+    // Abstract sub classes
+    static class Circle extends Shape {
+        
+        private double radius;
+
+        public Circle(double radius) {
+            this.radius = radius;
+        }
+
+        @Override
+        public double calculateArea() {
+            return Math.PI * radius * radius;
+        }
+    }
+
+    static class Rectangle extends Shape {
+
+        private double width;
+        private double height;
+
+        public Rectangle(double width, double height) {
+            this.width = width;
+            this.height = height;
+        }
+
+        @Override
+        public double calculateArea() {
+            return width * height;
+        }
+    }
+}
