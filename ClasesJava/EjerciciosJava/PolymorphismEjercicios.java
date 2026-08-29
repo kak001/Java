f677f271e7c086975bd4a8096981190352381b28
kak001 <joaco.atj06@gmail.com>
2026-08-29 14:05:20 -0400

    .java changed

--- ClasesJava/EjerciciosJava/PolymorphismEjercicios.java
+++ ClasesJava/EjerciciosJava/PolymorphismEjercicios.java
@@ -23,6 +23,31 @@ public class PolymorphismEjercicios {
             System.out.println("Area de la figura: "+ shapesList.calculateArea());
         }
         System.out.println();
+
+        // 3.
+        var varA = new Printer();
+        System.out.println("Mi nombre: " + varA.print("kako"));
+
+        var varB = new Printer();
+        System.out.println("Mi edad: " + varB.print(20));
+
+        var varC = new Printer();
+        System.out.println("Numero Pi (Aproximado): " + varC.print(3.14));
+
+        var varD = new Printer();
+        System.out.println("Mi inicial: " + varD.print('J'));
+
+        var varE = new Printer();
+        System.out.println("¿Me gusta programar?: " + varE.print(true));
+        System.out.println();
+
+        // 4.
+        var greeter = new Greeter();
+        greeter.greet();
+        System.out.println(greeter.greet("kako"));
+        System.out.println();
+
+        // 5.
     }
     
     // 1. Crea una clase Animal con el método makeSound(). Luego crea subclases Dog, Cat y Cow que sobrescriban ese método con sonidos diferentes. Llama al método desde una lista de Animal.
@@ -87,5 +112,39 @@ public class PolymorphismEjercicios {
         public double calculateArea() {
             return width * height;
         }
+    }
+    
+    // 3. Crea una clase Printer con varios métodos print() sobrecargados que acepten diferentes tipos de parámetros (String, int, double). Llama a cada uno desde main.
+    public static class Printer {
+        public String print(String a) {
+            return a;
+        }
+
+        public int print(int b) {
+            return + b;
+        } 
+        
+        public double print(double c) {
+            return c;
+        }
+
+        public char print(char d) {
+            return d;
+        }
+
+        public boolean print(boolean e) {
+            return e;
+        }
+    }
+    
+    // 4. Crea una clase Greeter con dos métodos greet(): uno que salude con “Hello”, y otro que reciba un nombre y salude con “Hello, [nombre]”.
+    public static class Greeter {
+        public void greet() {
+            System.out.println("Hello");
+        }
+
+        public String greet(String name) {
+            return "Hello, " + name;
+        } 
     }
 }
